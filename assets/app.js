19f9fe96e786c3a9949e2e5df91f73be35f466dd
boymtp12 <86153679+boymtp12@users.noreply.github.com>
2021-12-21 11:22:27 +0700

    adding my files to my respository

---
+++ assets/app.js
@@ -0,0 +1,7 @@
+openSubnavFree = document.querySelector('.open-subnav-freeMobie');
+menuSectionFree = document.querySelector('.menu-section');
+
+
+openSubnavFree.onclick = function() {
+    menuSectionFree.classList.add('open');
+}
